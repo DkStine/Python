93c9edaf16472335ae0fb204dc2022ef4157f3b9
DkStine <dkctcak@gmail.com>
2023-08-13 19:33:27 +0530

    commit

--- CodeChef/02.py
+++ CodeChef/02.py
@@ -1,7 +1,13 @@
+import sys
+sys.setrecursionlimit(10**8)
+
 class Solution:
     def nthFibonacci(self, n : int) -> int:
         # code here
-        if n == 1 or n == 2:
-            return 1
-        else:
-            return {nthFibonacci(n - 1) + nthFibonacci(n - 2)}
+        if n==1 or n==0:
+            return n
+        
+        return (self.nthFibonacci(n - 1) + self.nthFibonacci(n - 2)) % 1000000007
+        
+k = Solution()
+print(k.nthFibonacci(14521))
