b6f83366989180ca4ea8774e3417ecc5e429ca1f
DkStine <dkctcak@gmail.com>
2023-09-12 15:56:19 +0530

    PassCheck

--- Woxsen/01.py
+++ Woxsen/01.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+# print("Hello World!")
+
+passwd = input('Enter the password: ')
+passwd_length = 0
+digit = False
+
+for i in passwd:
+    passwd_length = passwd_length + 1
+    if i.isdigit():
+        digit = True
+    else:
+        digit = False
+
+if (passwd_length >= 8) and (digit == True):
+    print("Valid!")
+else:
+    print("Invalid")
