4669c3e8d4d29b7aceef11c59b9cdedf1c231d97
DkStine <dkctcak@gmail.com>
2023-10-01 23:29:33 +0530

    StopwordRemoval

--- Woxsen/01.py
+++ Woxsen/01.py
@@ -1,17 +1,24 @@
 # print("Hello World!")
 
-passwd = input('Enter the password: ')
-passwd_length = 0
-digit = False
+# passwd = input('Enter the password: ')
+# passwd_length = 0
+# digit = False
 
-for i in passwd:
-    passwd_length = passwd_length + 1
-    if i.isdigit():
-        digit = True
-    else:
-        digit = False
+# for i in passwd:
+#     passwd_length = passwd_length + 1
+#     if i.isdigit():
+#         digit = True
+#     else:
+#         digit = False
 
-if (passwd_length >= 8) and (digit == True):
-    print("Valid!")
-else:
-    print("Invalid")
+# if (passwd_length >= 8) and (digit == True):
+#     print("Valid!")
+# else:
+#     print("Invalid")
+
+dic = "Hello World"
+
+for i in dic:
+    if i.isalpha():
+        print(i)
+    else: break
